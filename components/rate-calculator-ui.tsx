3759641e990b438305861487e4e62a66a717502a
MarcoRigazio <marco@marcorigaziovo.com>
2025-11-01 14:14:15 -0400

    Replace console log with addToCart function

--- components/rate-calculator-ui.tsx
+++ components/rate-calculator-ui.tsx
@@ -2966,7 +2966,7 @@ export function RateCalculatorUI() {
                   </p>
                 </div>
                 <button
-                 onClick={() => console.log('Add to cart')}
+                 onClick={addToCart}
                  className="w-full mt-4 p-3 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700 transition-colors"
                 >
                  Add to Project Quote
