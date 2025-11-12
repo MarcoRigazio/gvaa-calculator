7611b0f11a8952a3f552eb64ddefb1d4545a0610
MarcoRigazio <marco@marcorigaziovo.com>
2025-11-12 10:20:41 -0500

    Replace RateCalculatorUI with NewUIPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-// app/page.tsx
+import NewUIPage from "./new-ui-page";
 
-// 1. Import the NAMED export { RateCalculatorUI } using curly braces
-// 2. Point to the correct file in this folder: "./homepage-backup"
-import { RateCalculatorUI } from "./homepage-backup";
-
-// 3. Render that component
 export default function HomePage() {
-  return <RateCalculatorUI />;
+  return <NewUIPage />;
 }
