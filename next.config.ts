d73b6e21c5281c69f317336d498ae8ad38524931
MarcoRigazio <marco@marcorigaziovo.com>
2025-12-06 15:45:58 -0500

    Add experimental server actions to next.config.ts
    Added experimental server actions configuration with allowed origins.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "http://localhost:3000",
+        "https://jubilant-waddle-7vj79w57p99r2xpgr-3000.app.github.dev",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
