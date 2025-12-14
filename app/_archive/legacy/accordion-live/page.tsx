d1ad687ea5e0aa75713feee60601a2bf9dbe4a03
Marco Rigazio <marco@marcorigaziovo.com>
2025-12-14 13:52:20 -0500

    hotfix: disable legacy accordion-live page to unblock build

--- app/_archive/legacy/accordion-live/page.tsx
+++ app/_archive/legacy/accordion-live/page.tsx
@@ -1,9 +1,3 @@
-import { CalculatorAccordionWrapper } from "@/components/rate-calculator-ui/CalculatorAccordionWrapper";
-
 export default function AccordionLivePage() {
-  return (
-    <div className="p-6">
-      <CalculatorAccordionWrapper />
-    </div>
-  );
+  return null;
 }
